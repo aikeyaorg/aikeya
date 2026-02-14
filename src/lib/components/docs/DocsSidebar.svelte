63702f66bafe254c655dfc00211df85cbf18a59f
0xMerl <merl990827@gmail.com>
2026-02-14 22:37:29 +0800

    Add files via upload

--- src/lib/components/docs/DocsSidebar.svelte
+++ src/lib/components/docs/DocsSidebar.svelte
@@ -52,7 +52,7 @@
         		<path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"/>
     		</svg>
 		</a>
-		<a href="https://pump.fun" target="_blank" rel="noopener noreferrer" class="footer-btn" title="pump.fun">
+		<a href="https://pump.fun/coin/3xxHPHvpiSxFcdg65CaorjjSfsqiBWoUWLLeUimTpump" target="_blank" rel="noopener noreferrer" class="footer-btn" title="pump.fun">
     		<Icon name="zap" size={14} />
 		</a>
 	</div>
